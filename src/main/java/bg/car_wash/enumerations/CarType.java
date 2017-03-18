2aaf6b78c79b960272b811a6526513e8a6fb46fa
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-03-18 21:39:59 +0200

    create enumeration for user and car

---
+++ src/main/java/bg/car_wash/enumerations/CarType.java
@@ -0,0 +1,5 @@
+package bg.car_wash.enumerations;
+
+public enum CarType {
+	CAR, VAN, JEEP, BUS, PICAP
+}
---
+++ src/main/java/bg/car_wash/enumerations/UserType.java
@@ -0,0 +1,5 @@
+package bg.car_wash.enumerations;
+
+public enum UserType {
+	WORKER, ADMIN, DIRECTOR, PAYMASTER
+}
