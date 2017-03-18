64b1295303f2f41a328a5e2cadb456054a6512f7
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-03-18 21:40:33 +0200

    create customer entity

---
+++ src/main/java/bg/car_wash/entities/Customer.java
@@ -0,0 +1,75 @@
+package bg.car_wash.entities;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name", nullable = false)
+	@Size(min = 3, max = 100)
+	private String name;
+
+	@Column(name = "phone_number", nullable = false)
+	@Size(min = 10, max = 15)
+	private String phoneNumber;
+
+	@Basic
+	private Date date;
+
+	@Basic
+	private int discount;
+
+	@OneToMany(targetEntity = Car.class, cascade = CascadeType.ALL)
+	private List<Car> cars;
+
+	public Customer() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+}
