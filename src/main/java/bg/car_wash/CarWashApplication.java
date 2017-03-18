7ae961378738d7aa8ce68964c75474222958b68e
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-03-18 18:31:59 +0200

    create project

---
+++ src/main/java/bg/car_wash/CarWashApplication.java
@@ -0,0 +1,12 @@
+package bg.car_wash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarWashApplication.class, args);
+	}
+}
