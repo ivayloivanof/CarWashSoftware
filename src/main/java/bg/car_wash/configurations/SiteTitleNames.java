ff437740eb9d7dc051df6725277060d1e6a59d99
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-03-19 16:39:38 +0200

    add Site and User configuration files

---
+++ src/main/java/bg/car_wash/configurations/SiteTitleNames.java
@@ -0,0 +1,9 @@
+package bg.car_wash.configurations;
+
+public final class  SiteTitleNames {
+
+	public static final String HOME_PAGE = "Home page";
+	public static final String LOGIN_PAGE = "Login page";
+	public static final String REGISTER_PAGE = "Register page";
+
+}
