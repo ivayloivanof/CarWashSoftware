0d413df3aa573439d9f03fac0ddb9b794a162931
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-03-19 16:39:57 +0200

    update homo controller

--- src/main/java/bg/car_wash/controllers/HomeController.java
+++ src/main/java/bg/car_wash/controllers/HomeController.java
@@ -1,13 +1,16 @@
 package bg.car_wash.controllers;
 
+import bg.car_wash.configurations.SiteTitleNames;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
 	@GetMapping("/")
-	public String getHomePage() {
+	public String getHomePage(Model model) {
+		model.addAttribute("pageTitle", SiteTitleNames.HOME_PAGE);
 		return "home";
 	}
 
