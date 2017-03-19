4aecebd9b3f371434b49d8f57175359a9faa445f
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-03-19 16:40:56 +0200

    create user repository

---
+++ src/main/java/bg/car_wash/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package bg.car_wash.repositories;
+
+import bg.car_wash.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+//	@Query("SELECT u FROM User AS u WHERE u.email = :email AND u.password = :password")
+	User findUserByEmailAndPassword(@Param("email") String email, @Param("password") String password);
+
+	List<User> findAll();
+}
