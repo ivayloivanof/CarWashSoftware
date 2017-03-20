b20436a3fc4c85b65a4aa787a962fe452e3b6f08
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-03-20 06:29:09 +0200

    add new name in site title

--- src/main/java/bg/car_wash/configurations/SiteTitleNames.java
+++ src/main/java/bg/car_wash/configurations/SiteTitleNames.java
@@ -5,5 +5,6 @@ public final class  SiteTitleNames {
 	public static final String HOME_PAGE = "Home page";
 	public static final String LOGIN_PAGE = "Login page";
 	public static final String REGISTER_PAGE = "Register page";
+	public static final String USER_STATUS_PAGE = "User status page";
 
 }
