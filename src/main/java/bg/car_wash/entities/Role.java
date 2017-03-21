aac291820d109f82c18dd6e73e549dbe6e295741
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-03-21 21:12:21 +0200

    create Role entity

---
+++ src/main/java/bg/car_wash/entities/Role.java
@@ -0,0 +1,34 @@
+package bg.car_wash.entities;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Basic
+    private String authority;
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
