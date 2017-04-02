6f334d0ffed3f868f302aa016f25b197d6975166
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-02 09:32:45 +0300

    update errors

--- src/main/java/bg/car_wash/configurations/Errors.java
+++ src/main/java/bg/car_wash/configurations/Errors.java
@@ -3,4 +3,5 @@ package bg.car_wash.configurations;
 public class Errors {
 
     public static final String INVALID_CREDENTIALS = "Invalid Credentials";
+    public static final String NOT_FOUND_USER = "Not found user";
 }
