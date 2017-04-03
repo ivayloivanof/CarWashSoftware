81e22e65ad0fc90c4a399798b1580287d822c1b9
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-03 06:59:10 +0300

    create page url class with const

---
+++ src/main/java/bg/car_wash/configurations/site/PageUrl.java
@@ -0,0 +1,12 @@
+package bg.car_wash.configurations.site;
+
+public class PageUrl {
+	public static final String HOME_PAGE = "/";
+	public static final String LOGIN_PAGE = "/user/login";
+	public static final String LOGOUT_PAGE = "/user/logout";
+	public static final String REGISTER_PAGE = "/user/register";
+	public static final String USER_STATUS_PAGE = "/user/status";
+	public static final String CAR_ALL_PAGE = "/car/all";
+	public static final String CAR_ADD_FOR_WORK_PAGE = "/car/add";
+
+}
