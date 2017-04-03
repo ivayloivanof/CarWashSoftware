2bb06a471c895d20f687c91a3c5c08dea8c01783
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-03 05:49:58 +0300

    create 4 users with roles

--- src/main/java/bg/car_wash/configurations/database/UserDefault.java
+++ src/main/java/bg/car_wash/configurations/database/UserDefault.java
@@ -2,6 +2,8 @@ package bg.car_wash.configurations.database;
 
 import bg.car_wash.areas.user.entity.User;
 import bg.car_wash.areas.user.entity.UserType;
+import bg.car_wash.configurations.user.UserConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.math.BigDecimal;
 import java.util.LinkedList;
@@ -9,42 +11,63 @@ import java.util.List;
 
 public class UserDefault {
 
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	private String password;
+
 	private List<User> users;
 
 	public UserDefault() {
 		this.users = new LinkedList<>();
+		this.bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		this.password = this.bCryptPasswordEncoder.encode("123456");
 		this.initUsers();
 	}
 
 	public void initUsers() {
 		User u1 = new User();
 		u1.setEmail("ivan@abv.bg");
-		u1.setUsername("ivan");
 		u1.setFullName("Ivan Ivanov");
-		u1.setPassword("123456");
+		u1.setPassword(this.password);
 		u1.setUserType(UserType.ADMIN);
-		u1.setRemuneration(new BigDecimal("1000"));
+		u1.setRemuneration(new BigDecimal(UserConfiguration.ADMIN_REMUNERATION));
+		u1.setAccountNonExpired(true);
+		u1.setAccountNonLocked(true);
+		u1.setCredentialsNonExpired(true);
+		u1.setEnabled(true);
+
 		User u2 = new User();
 		u2.setEmail("pesho@abv.bg");
-		u2.setUsername("pesho");
 		u2.setFullName("Pesho Peshev");
-		u2.setPassword("123456");
+		u2.setPassword(this.password);
 		u2.setUserType(UserType.DIRECTOR);
-		u2.setRemuneration(new BigDecimal("2000"));
+		u2.setRemuneration(new BigDecimal(UserConfiguration.DIRECTOR_REMUNERATION));
+		u2.setAccountNonExpired(true);
+		u2.setAccountNonLocked(true);
+		u2.setCredentialsNonExpired(true);
+		u2.setEnabled(true);
+
 		User u3 = new User();
 		u3.setEmail("asen@abv.bg");
-		u3.setUsername("asen");
 		u3.setFullName("Asen Asenov");
-		u3.setPassword("123456");
+		u3.setPassword(this.password);
 		u3.setUserType(UserType.WORKER);
-		u3.setRemuneration(new BigDecimal("600"));
+		u3.setRemuneration(new BigDecimal(UserConfiguration.WORKER_REMUNERATION));
+		u3.setAccountNonExpired(true);
+		u3.setAccountNonLocked(true);
+		u3.setCredentialsNonExpired(true);
+		u3.setEnabled(true);
+
 		User u4 = new User();
 		u4.setEmail("krum@abv.bg");
-		u4.setUsername("krum");
 		u4.setFullName("Krum Krumov");
-		u4.setPassword("123456");
+		u4.setPassword(this.password);
 		u4.setUserType(UserType.PAYMASTER);
-		u4.setRemuneration(new BigDecimal("800"));
+		u4.setRemuneration(new BigDecimal(UserConfiguration.PAYMASTER_REMUNERATION));
+		u4.setAccountNonExpired(true);
+		u4.setAccountNonLocked(true);
+		u4.setCredentialsNonExpired(true);
+		u4.setEnabled(true);
 
 		this.users.add(u1);
 		this.users.add(u2);
