733c2367872691595f4ec1c3142660ab98a8cf36
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-03 05:49:25 +0300

    using errors class

--- src/main/java/bg/car_wash/areas/user/exception/UserNotFoundException.java
+++ src/main/java/bg/car_wash/areas/user/exception/UserNotFoundException.java
@@ -1,8 +1,9 @@
 package bg.car_wash.areas.user.exception;
 
+import bg.car_wash.configurations.error.Errors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE, reason = "Not foud user")
+@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE, reason = Errors.NOT_FOUND_USER)
 public class UserNotFoundException extends RuntimeException {
 }
