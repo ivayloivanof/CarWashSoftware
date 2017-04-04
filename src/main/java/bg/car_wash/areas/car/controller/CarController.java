bb0378b7076d075d7c8c54b52e34df33cc0cfcc2
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-04 06:18:14 +0300

    create add form and edit and delete get mapping in car controller

--- src/main/java/bg/car_wash/areas/car/controller/CarController.java
+++ src/main/java/bg/car_wash/areas/car/controller/CarController.java
@@ -1,10 +1,15 @@
 package bg.car_wash.areas.car.controller;
 
+import bg.car_wash.areas.car.models.bindingModel.CarBindingModel;
 import bg.car_wash.configurations.site.PageTitle;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/car")
@@ -17,8 +22,23 @@ public class CarController {
 	}
 
 	@GetMapping("/add")
-	public String addCarForWorkPage(Model model) {
-		model.addAttribute("pageTitle", PageTitle.CAR_ADD_FOR_WORK_PAGE);
+	public String addCarForWorkPage(
+			Model model,
+			@Valid @ModelAttribute CarBindingModel carBindingModel,
+			BindingResult bindingResult) {
+		model.addAttribute("pageTitle", PageTitle.CAR_ADD_PAGE);
 		return "car/car-add";
+	}
+
+	@GetMapping("/edit")
+	public String editCarForWorkPage(Model model) {
+		model.addAttribute("pageTitle", PageTitle.CAR_EDIT_PAGE);
+		return "car/car-edit";
+	}
+
+	@GetMapping("/delete")
+	public String deleteCarForWorkPage(Model model) {
+		model.addAttribute("pageTitle", PageTitle.CAR_DELETE_PAGE);
+		return "car/car-edit";
 	}
 }
