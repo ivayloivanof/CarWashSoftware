f15f1083e82b846fe6d387474df97c9cb229b287
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-06 06:58:00 +0300

    update "all" and "add" paths in car controller

--- src/main/java/bg/car_wash/areas/car/controller/CarController.java
+++ src/main/java/bg/car_wash/areas/car/controller/CarController.java
@@ -1,23 +1,46 @@
 package bg.car_wash.areas.car.controller;
 
+import bg.car_wash.areas.car.entity.Car;
 import bg.car_wash.areas.car.models.bindingModel.CarBindingModel;
+import bg.car_wash.areas.car.models.viewModel.CarViewModel;
+import bg.car_wash.areas.car.service.CarService;
 import bg.car_wash.configurations.site.PageTitle;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/car")
 public class CarController {
 
+	@Autowired
+	private CarService carService;
+
+	@Autowired
+	private ModelMapper modelMapper;
+
 	@GetMapping("/all")
 	public String getAllCarsPage(Model model) {
 		model.addAttribute("pageTitle", PageTitle.CAR_ALL_PAGE);
+
+		List<Car> cars = this.carService.findAllCars();
+		List<CarViewModel> carsViewModel = new LinkedList<>();
+		for (Car car : cars) {
+			carsViewModel.add(this.modelMapper.map(car, CarViewModel.class));
+		}
+
+		model.addAttribute("carViewModel", carsViewModel);
+
 		return "car/car-all";
 	}
 
@@ -28,6 +51,26 @@ public class CarController {
 			BindingResult bindingResult) {
 		model.addAttribute("pageTitle", PageTitle.CAR_ADD_PAGE);
 		return "car/car-add";
+	}
+
+	@PostMapping("/add")
+	public String addCar(
+			Model model,
+			@Valid @ModelAttribute CarBindingModel carBindingModel,
+			BindingResult bindingResult) {
+		model.addAttribute("pageTitle", PageTitle.CAR_ADD_PAGE);
+
+		if(bindingResult.hasErrors()) {
+			return "car/car-add";
+		}
+
+		//TODO get customer from DB
+		Car car = this.modelMapper.map(carBindingModel, Car.class);
+		car.setCarModelName(carBindingModel.getCarModel());
+
+		this.carService.createCar(car);
+
+		return "redirect:/car/all";
 	}
 
 	@GetMapping("/edit")
