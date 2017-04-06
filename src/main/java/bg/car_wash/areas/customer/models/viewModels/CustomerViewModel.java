a65473d9044b142d10573c89df567c837db84129
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-06 06:54:00 +0300

    create customer view model

---
+++ src/main/java/bg/car_wash/areas/customer/models/viewModels/CustomerViewModel.java
@@ -0,0 +1,59 @@
+package bg.car_wash.areas.customer.models.viewModels;
+
+import java.util.Date;
+
+public class CustomerViewModel {
+
+	private Long id;
+
+	private String name;
+
+	private String phoneNumber;
+
+	private Date date;
+
+	private int discount;
+
+	public CustomerViewModel() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+}
