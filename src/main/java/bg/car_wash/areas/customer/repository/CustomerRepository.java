c44b1ae69bdbf640a1042dc00d7ca0b2ad163768
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-07 06:57:40 +0300

    create customer repository

---
+++ src/main/java/bg/car_wash/areas/customer/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package bg.car_wash.areas.customer.repository;
+
+import bg.car_wash.areas.customer.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+	Customer findCustomerByName(String name);
+	Customer findCustomerByPhoneNumber(String phone);
+}
