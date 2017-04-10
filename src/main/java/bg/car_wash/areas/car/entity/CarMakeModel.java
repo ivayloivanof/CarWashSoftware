21b8ebc5a47f169361970be1ef45703bb1bbe28b
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-10 07:06:43 +0300

    create carModelMake entity

---
+++ src/main/java/bg/car_wash/areas/car/entity/CarMakeModel.java
@@ -0,0 +1,48 @@
+package bg.car_wash.areas.car.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "car_make_model")
+public class CarMakeModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "car_make", nullable = false)
+	@Size(min = 2, max = 40)
+	private String make;
+
+	@Column(name = "car_model", nullable = false)
+	@Size(min = 1, max = 40)
+	private String model;
+
+	public CarMakeModel() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+}
