8d5c450d650ad85df4fb07fcfc0a69a0a8c508d6
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-10 07:05:38 +0300

    create all links in menu for site CarWashSoftware

--- src/main/java/bg/car_wash/configurations/site/PageTitle.java
+++ src/main/java/bg/car_wash/configurations/site/PageTitle.java
@@ -3,13 +3,33 @@ package bg.car_wash.configurations.site;
 public final class PageTitle {
 
 	public static final String HOME_PAGE = "Home";
+
 	public static final String LOGIN_PAGE = "Login";
 	public static final String LOGOUT_PAGE = "Logout";
 	public static final String REGISTER_PAGE = "Register";
 	public static final String USER_STATUS_PAGE = "User status";
+
 	public static final String CAR_ALL_PAGE = "All cars";
+	public static final String CAR_INFO_PAGE = "User info";
 	public static final String CAR_ADD_PAGE = "Add car";
 	public static final String CAR_EDIT_PAGE = "Edit car";
 	public static final String CAR_DELETE_PAGE = "Delete car";
 
+	public static final String CUSTOMER_ALL_PAGE = "Customers";
+	public static final String CUSTOMER_INFO_PAGE = "Customer info";
+	public static final String CUSTOMER_ADD_PAGE = "Add customer";
+	public static final String CUSTOMER_EDIT_PAGE = "Edit customer";
+	public static final String CUSTOMER_DELETE_PAGE = "Delete customer";
+
+	public static final String ACTIVITY_ALL_PAGE = "Activities";
+	public static final String ACTIVITY_INFO_PAGE = "Activity info";
+	public static final String ACTIVITY_ADD_PAGE = "Add activity";
+	public static final String ACTIVITY_EDIT_PAGE = "Edit activity";
+	public static final String ACTIVITY_DELETE_PAGE = "Delete activity";
+
+	public static final String SERVICE_ALL_PAGE = "Services";
+	public static final String SERVICE_INFO_PAGE = "Service info";
+	public static final String SERVICE_ADD_PAGE = "Add service";
+	public static final String SERVICE_EDIT_PAGE = "Edit service";
+	public static final String SERVICE_DELETE_PAGE = "Delete service";
 }
--- src/main/java/bg/car_wash/configurations/site/PageUrl.java
+++ src/main/java/bg/car_wash/configurations/site/PageUrl.java
@@ -2,13 +2,34 @@ package bg.car_wash.configurations.site;
 
 public class PageUrl {
 	public static final String HOME_PAGE = "/";
+
 	public static final String LOGIN_PAGE = "/user/login";
 	public static final String LOGOUT_PAGE = "/user/logout";
 	public static final String REGISTER_PAGE = "/user/register";
 	public static final String USER_STATUS_PAGE = "/user/status";
+
 	public static final String CAR_ALL_PAGE = "/car/all";
+	public static final String CAR_INFO_PAGE = "/car/info";
 	public static final String CAR_ADD_PAGE = "/car/add";
 	public static final String CAR_EDIT_PAGE = "/car/edit";
 	public static final String CAR_DELETE_PAGE = "/car/delete";
+
+	public static final String CUSTOMER_ALL_PAGE = "/customer/all";
+	public static final String CUSTOMER_INFO_PAGE = "/customer/info";
+	public static final String CUSTOMER_ADD_PAGE = "/customer/add";
+	public static final String CUSTOMER_EDIT_PAGE = "/customer/edit";
+	public static final String CUSTOMER_DELETE_PAGE = "/customer/delete";
+
+	public static final String ACTIVITY_ALL_PAGE = "/activity/all";
+	public static final String ACTIVITY_INFO_PAGE = "/activity/info";
+	public static final String ACTIVITY_ADD_PAGE = "/activity/add";
+	public static final String ACTIVITY_EDIT_PAGE = "/activity/edit";
+	public static final String ACTIVITY_DELETE_PAGE = "/activity/delete";
+
+	public static final String SERVICE_ALL_PAGE = "/service/all";
+	public static final String SERVICE_INFO_PAGE = "/service/info";
+	public static final String SERVICE_ADD_PAGE = "/service/add";
+	public static final String SERVICE_EDIT_PAGE = "/service/edit";
+	public static final String SERVICE_DELETE_PAGE = "/service/delete";
 
 }
