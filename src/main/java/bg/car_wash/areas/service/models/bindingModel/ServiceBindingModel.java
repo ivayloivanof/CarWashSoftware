6a927fa737066fb70c5e5f0fcff66f330fd8c6a0
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-14 20:01:23 +0300

    create ServiceBindingModel

---
+++ src/main/java/bg/car_wash/areas/service/models/bindingModel/ServiceBindingModel.java
@@ -0,0 +1,58 @@
+package bg.car_wash.areas.service.models.bindingModel;
+
+import bg.car_wash.areas.car.entity.CarType;
+import bg.car_wash.areas.user.entity.UserType;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+
+public class ServiceBindingModel {
+
+	@Size(min = 3, max = 50, message = "Service name between 3 and 50 characters!")
+	private String serviceName;
+
+	@Digits(integer = 2, fraction = 2)
+	@DecimalMin(value = "0.00", message = "Service price can not be negative number!")
+	private BigDecimal servicePrice;
+
+	private CarType carType;
+
+	private UserType userType;
+
+	public ServiceBindingModel() {
+	}
+
+	public String getServiceName() {
+		return serviceName;
+	}
+
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+
+	public BigDecimal getServicePrice() {
+		return servicePrice;
+	}
+
+	public void setServicePrice(BigDecimal servicePrice) {
+		this.servicePrice = servicePrice;
+	}
+
+	public CarType getCarType() {
+		return carType;
+	}
+
+	public void setCarType(CarType carType) {
+		this.carType = carType;
+	}
+
+	public UserType getUserType() {
+		return userType;
+	}
+
+	public void setUserType(UserType userType) {
+		this.userType = userType;
+	}
+}
