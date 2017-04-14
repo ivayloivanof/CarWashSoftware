2b632d03b995a9b37bd3fa06252d249a7e3df665
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-14 19:57:41 +0300

    create activity exceptions

---
+++ src/main/java/bg/car_wash/areas/activity/exception/ActivityDBEmptyException.java
@@ -0,0 +1,33 @@
+package bg.car_wash.areas.activity.exception;
+
+/**
+ * Created by ivanof on 14.04.17.
+ */
+public class ActivityDBEmptyException extends RuntimeException {
+
+	private String message;
+
+	private int code;
+
+	public ActivityDBEmptyException(String message, int code) {
+		this.setMessage(message);
+		this.setCode(code);
+	}
+
+	@Override
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+}
