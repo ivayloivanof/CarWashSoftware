935361ce2eccae8986a085045a3aa4a3f2bcfcbb
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-14 19:56:48 +0300

    create ActivityBindingModel

---
+++ src/main/java/bg/car_wash/areas/activity/models/bindingModel/ActivityBindingModel.java
@@ -0,0 +1,35 @@
+package bg.car_wash.areas.activity.models.bindingModel;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+
+public class ActivityBindingModel {
+
+	@Size(min = 3, max = 40, message = "Invalid Activity name. (Activity name between 3 and 40 characters!")
+	private String activityName;
+
+	@Digits(integer = 2, fraction = 2)
+	@DecimalMin(value = "0.00", message = "Activity price can not be negative number!")
+	private BigDecimal activityPrice;
+
+	public ActivityBindingModel() {
+	}
+
+	public String getActivityName() {
+		return activityName;
+	}
+
+	public void setActivityName(String activityName) {
+		this.activityName = activityName;
+	}
+
+	public BigDecimal getActivityPrice() {
+		return activityPrice;
+	}
+
+	public void setActivityPrice(BigDecimal activityPrice) {
+		this.activityPrice = activityPrice;
+	}
+}
