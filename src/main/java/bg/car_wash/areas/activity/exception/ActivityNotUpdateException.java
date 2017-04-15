edb2471dfaf29f27f38abd723826f1363a0d1ccd
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-15 20:03:26 +0300

    create ActivityNotUpdateException

---
+++ src/main/java/bg/car_wash/areas/activity/exception/ActivityNotUpdateException.java
@@ -0,0 +1,30 @@
+package bg.car_wash.areas.activity.exception;
+
+public class ActivityNotUpdateException extends Exception {
+
+	private String message;
+
+	private int code;
+
+	public ActivityNotUpdateException(String message, int code) {
+		this.setMessage(message);
+		this.setCode(code);
+	}
+
+	@Override
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+}
