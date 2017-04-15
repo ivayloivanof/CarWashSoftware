168f9f6a6060643f9f79b05cb32071399d57a171
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-15 14:14:06 +0300

    create CustomerAddFormBindingModel

---
+++ src/main/java/bg/car_wash/areas/customer/models/bindingModel/CustomerAddFormBindingModel.java
@@ -0,0 +1,17 @@
+package bg.car_wash.areas.customer.models.bindingModel;
+
+public class CustomerAddFormBindingModel {
+
+	private Long id;
+
+	public CustomerAddFormBindingModel() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
