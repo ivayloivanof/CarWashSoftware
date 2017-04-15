577c9e758713ff3c4ee0dee5b8437fe4dca8b579
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-15 20:04:45 +0300

    add method findActivityById(Long id) and deleteActivityById(Long id);

--- src/main/java/bg/car_wash/areas/activity/repository/ActivityRepository.java
+++ src/main/java/bg/car_wash/areas/activity/repository/ActivityRepository.java
@@ -4,7 +4,12 @@ import bg.car_wash.areas.activity.entity.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
 	Activity findByActivityName(String name);
+	Activity findActivityById(Long id);
+	@Transactional
+	void deleteActivityById(Long id);
 }
