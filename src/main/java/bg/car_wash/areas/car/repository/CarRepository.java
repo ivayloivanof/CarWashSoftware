4fe94a7012c9c803ae2a24ca1930884f823d1cb6
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-15 19:07:00 +0300

    add @Transactional - throw exception without annotation

--- src/main/java/bg/car_wash/areas/car/repository/CarRepository.java
+++ src/main/java/bg/car_wash/areas/car/repository/CarRepository.java
@@ -4,12 +4,16 @@ import bg.car_wash.areas.car.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
 
 	Car findCarByCarRegistrationNumber(String registrationNUmber);
 
+	@Transactional
 	void deleteCarById(Long id);
 
+	@Transactional
 	Car findCarById(Long id);
 }
