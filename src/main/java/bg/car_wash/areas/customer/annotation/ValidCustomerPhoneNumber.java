51dc4cd3a45504dc0288e2b0a08568812be126e0
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-17 17:49:29 +0300

    create validation for phone number

---
+++ src/main/java/bg/car_wash/areas/customer/annotation/ValidCustomerPhoneNumber.java
@@ -0,0 +1,22 @@
+package bg.car_wash.areas.customer.annotation;
+
+import bg.car_wash.areas.customer.annotation.validation.CustomerPhoneNumberValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({TYPE, FIELD, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = CustomerPhoneNumberValidator.class)
+@Documented
+public @interface ValidCustomerPhoneNumber {
+	String message() default "This phone number is not valid! Phone number like: '+359 111 222 333'!";
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+}
