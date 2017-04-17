f7eb0d8a84cc820e63d191cd099aac474561f8e9
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-17 12:11:56 +0300

    create UserEditViewModel

---
+++ src/main/java/bg/car_wash/areas/user/models/viewModels/UserEditViewModel.java
@@ -0,0 +1,59 @@
+package bg.car_wash.areas.user.models.viewModels;
+
+import bg.car_wash.areas.user.entity.UserType;
+
+public class UserEditViewModel {
+
+	private Long id;
+
+	private String email;
+
+	private String fullName;
+
+	private String password;
+
+	private UserType userType;
+
+	public UserEditViewModel() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public UserType getUserType() {
+		return userType;
+	}
+
+	public void setUserType(UserType userType) {
+		this.userType = userType;
+	}
+}
