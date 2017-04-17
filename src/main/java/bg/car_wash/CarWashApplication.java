ca1979c8897fc4297fe9a1c5df608928481e1908
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-17 17:51:03 +0300

    insert default 6 users with all workers roles in database and insert many car model and make (1900-2000) in database

--- src/main/java/bg/car_wash/CarWashApplication.java
+++ src/main/java/bg/car_wash/CarWashApplication.java
@@ -1,7 +1,11 @@
 package bg.car_wash;
 
+import bg.car_wash.areas.car.entity.CarMakeModel;
+import bg.car_wash.areas.car.service.CarMakeModelService;
+import bg.car_wash.areas.user.entity.User;
 import bg.car_wash.areas.user.service.UserService;
 import bg.car_wash.configurations.database.InsertCars;
+import bg.car_wash.configurations.database.InsertUsers;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 
 import javax.annotation.PostConstruct;
 import java.io.FileNotFoundException;
+import java.util.List;
 
 @SpringBootApplication
 public class CarWashApplication {
@@ -17,19 +22,48 @@ public class CarWashApplication {
 	@Autowired
 	private UserService userService;
 
+	@Autowired
+	private CarMakeModelService carMakeModelService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CarWashApplication.class, args);
 	}
 
-//	@PostConstruct
-//	public void initUsers() throws FileNotFoundException {
-////		InsertUsers userDefault = new InsertUsers();
-////		for (User user : userDefault.getUsers()) {
-////			this.userService.createUser(user);
-////		}
-//
-//		InsertCars insertCars = new InsertCars();
-//	}
+	@PostConstruct
+	public void initUsers() throws FileNotFoundException {
+		//create 6 default users in DB
+		if (checkUserDbIsEmpty()) {
+			InsertUsers userDefault = new InsertUsers();
+			for (User user : userDefault.getUsers()) {
+				this.userService.createUser(user);
+			}
+		}
+
+		//create all cars with models and makes in DB
+		if(checkCarMakeModelDbIsEmpty()) {
+			InsertCars insertCars = new InsertCars();
+			List<CarMakeModel> carMakeModels = insertCars.readJsonFile();
+			for (CarMakeModel carMakeModel : carMakeModels) {
+				this.carMakeModelService.createCar(carMakeModel);
+			}
+		}
+	}
+
+	private boolean checkCarMakeModelDbIsEmpty() {
+		if(this.carMakeModelService.findAllCarMakeModel().isEmpty()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean checkUserDbIsEmpty() {
+		if (this.userService.findAllUsers().isEmpty()) {
+			return true;
+		}
+
+		return false;
+	}
 
 	@Bean
 	public ModelMapper getModelMapper() {
