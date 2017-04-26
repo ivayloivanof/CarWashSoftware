33e69a51d94ba4726186fe8fcdf69bd4cc199487
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-26 22:11:39 +0300

    create ajax request and refactoring html page

--- src/main/resources/static/js/request.js
+++ src/main/resources/static/js/request.js
@@ -1,23 +1,83 @@
-$(function($) {
-    let url = window.location.pathname;
-    if(url == "/car/all") {
-        ajaxCarAll();
-    }
-});
-
-function ajaxCarAll() {
-    $.ajax({
-        type : "GET",
-        url : "/json/car/all",
-        dataType : 'json',
-        success : function(cars) {
-            console.log("SUCCESS: ", cars);
-        },
-        error : function(e) {
-            console.log("ERROR: ", e);
-        },
-        done : function(e) {
-            console.log("DONE");
-        }
+function getAllCars() {
+    $.getJSON("/json/car/all").done(function(cars) {
+        console.log("cars: ", cars);
     });
 }
+
+function getAllCustomers() {
+    $.getJSON("/json/customer/all").done(function (customers) {
+        console.log("customers", customers);
+    })
+}
+
+function getAllCarsModel() {
+    $.getJSON("/json/car/model/all").done(function (carModels) {
+        console.log(carModels);
+    })
+}
+
+function getAllCarsMake() {
+    $.getJSON("/json/car/make/all").done(function (carMake) {
+        console.log(carMake);
+    })
+}
+
+function getValueFromModelInputField() {
+    $("#carModel").keyup(function(){
+        let carModel = this.value;
+        let make = $('#carMake').val();
+        $.getJSON("/json/car/" + make + "/model/" + carModel).done(function (carModels) {
+            $('#carModels').empty();
+            $.each(carModels, function( index, model ) {
+                $('#carModels').append('<p>' + model.model + '</p>');
+            });
+        })
+    });
+}
+
+function getValueFromMakeInputField() {
+    $("#carMake").keyup(function(){
+        let carMake = this.value;
+        $.getJSON("/json/car/make/" + carMake).done(function (carMakes) {
+            $('#carMakes').empty();
+            $.each(carMakes, function( index, make ) {
+                $('#carMakes').append('<p>' + make.make + '</p>').unique('#carMakes');
+            });
+        })
+    });
+}
+
+function getCarFromDbOnSearchPage() {
+    $("#carSearch").keyup(function(){
+        let registrationNumber = this.value;
+        $.getJSON("/json/car/search/" + registrationNumber).done(function (cars) {
+            $('#cars').empty();
+            $.each(cars, function( index, car ) {
+                console.log(car);
+                $('#cars')
+                    .append($('<a></a>').attr("href", "/car/edit/" + car.id)
+                    .append($('<p>' + car.carRegistrationNumber + '</p>')));
+            });
+        })
+    });
+}
+
+$(function($) {
+    let url = window.location.pathname;
+
+    if (url == "/car/all") {
+        getAllCars();
+    }
+
+    if(url == "/car/add") {
+        getValueFromModelInputField();
+        getValueFromMakeInputField();
+    }
+
+    if(url == '/car/edit') {
+        getCarFromDbOnSearchPage();
+    }
+
+
+    // getAllCustomers();
+});
