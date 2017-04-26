5ab6d52e2e4649d87978d74e48a9c21a5b3c54b4
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-04-26 22:09:36 +0300

    create carMakeViewModel and CarModelViewModel

---
+++ src/main/java/bg/car_wash/areas/car/models/viewModel/CarMakeViewModel.java
@@ -0,0 +1,27 @@
+package bg.car_wash.areas.car.models.viewModel;
+
+public class CarMakeViewModel {
+
+	private Long id;
+
+	private String make;
+
+	public CarMakeViewModel() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+}
---
+++ src/main/java/bg/car_wash/areas/car/models/viewModel/CarModelViewModel.java
@@ -0,0 +1,28 @@
+package bg.car_wash.areas.car.models.viewModel;
+
+public class CarModelViewModel {
+
+	private Long id;
+
+	private String model;
+
+	public CarModelViewModel() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+}
